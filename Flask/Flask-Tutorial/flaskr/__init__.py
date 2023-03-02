32b91a18fcd2c289a2281abd78740b3a341dcb38
egmp7 <deweyerick@gmail.com>
2023-03-02 15:17:40 -0600

    database init

--- Flask/Flask-Tutorial/flaskr/__init__.py
+++ Flask/Flask-Tutorial/flaskr/__init__.py
@@ -28,5 +28,9 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+    
+    # connect to database
+    from . import db
+    db.init_app(app)
 
     return app
