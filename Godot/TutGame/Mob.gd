86c76500fe51a5d0070a2c58f52987e7aa0268cf
egmp7 <deweyerick@gmail.com>
2023-03-22 13:43:15 -0500

    creating enemy

---
+++ Godot/TutGame/Mob.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+# Create random animation for enemies
+func _ready():
+	$AnimatedSprite2D.play()
+	var mob_types = $AnimatedSprite2D.get_sprite_frames().get_animation_names()
+	$AnimatedSprite2D.animation = mob_types[ randi() % mob_types.size() ]
+	
+
+# Free memory when enemy is out of screen
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
