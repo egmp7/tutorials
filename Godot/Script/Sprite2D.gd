79c1b0b4f688c023ec324ed91a4172140ae0d53b
egmp7 <deweyerick@gmail.com>
2023-03-22 11:24:42 -0500

    how script works completed

--- Godot/Script/Sprite2D.gd
+++ Godot/Script/Sprite2D.gd
@@ -3,7 +3,9 @@ extends Sprite2D
 var speed = 400
 var angular_speed = PI
 
+# gets call every frame
 func _process(delta):
+	
 	var direction = 0
 	if Input.is_action_pressed("ui_left"):
 		direction = -1
@@ -12,5 +14,21 @@ func _process(delta):
 		direction = 1
 	
 	rotation += angular_speed * direction * delta
-	var velocity = Vector2.UP.rotated(rotation) * speed
+	
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
+	
 	position += velocity * delta
+
+# set process when pressing button
+func _on_button_pressed():
+	set_process(not is_processing())
+
+# get's called when node has been instantiated
+func _ready():
+	var timer = get_node("Timer")
+	timer.timeout.connect(_on_Timer_timeout)
+	
+func _on_Timer_timeout():
+	visible = not visible
