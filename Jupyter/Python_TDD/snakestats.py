81b05d691adedac64b41c3c4d8e0c664cb53ff0d
egmp7 <deweyerick@gmail.com>
2023-04-22 15:08:50 -0500

    unit testing getMin function

--- Jupyter/Python_TDD/snakestats.py
+++ Jupyter/Python_TDD/snakestats.py
@@ -7,3 +7,15 @@ def count(vals):
             count += 1
         
         return count
+    
+def getMin(vals):
+    
+    for i in range(len(vals)):
+        if isinstance(vals[i],str):
+            vals[i]= int(vals[i])
+    
+    min = vals[0]
+    for x in vals:
+        if x < min:
+            min = x
+    return min
