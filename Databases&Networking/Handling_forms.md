430fe4195ac45b62ef0415ae36b8ae27510ecbef
egmp7 <deweyerick@gmail.com>
2023-04-29 14:04:18 -0500

    week 7

---
+++ Databases&Networking/Handling_forms.md
@@ -0,0 +1,8 @@
+# Forms
+
+Forms are very important for data manipulation and allow you to collect, update and verify many
+information
+
+# HTTP methods
+
+![](/Databases&Networking/assets/15.png)
--- Databases&Networking/UoL/topic4/htmlExpress/routes/main.js
+++ Databases&Networking/UoL/topic4/htmlExpress/routes/main.js
@@ -1,1 +1,25 @@
-// The main.js file of your application
+module.exports = function(app) {
+    app.get("/",function(req, res){
+    
+        // dynamic page
+        res.render("index.html", {
+        title: "index"
+        });
+    });
+
+    app.get("/about",function(req, res){
+    
+        // dynamic page
+        res.render("about.html", {
+        title: "About"
+        });
+    });
+
+    app.get("/search",function(req, res){
+    
+        // dynamic page
+        res.render("search.html", {
+        title: "search"
+        });
+    });
+}
