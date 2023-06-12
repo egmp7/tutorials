3a9700f8f41fb4484ecccf36cb7f166091dc4bfb
egmp7 <deweyerick@gmail.com>
2023-06-12 13:53:32 -0500

    connecting dots working

--- JS/p5/wavingDots/sketch.js
+++ JS/p5/wavingDots/sketch.js
@@ -15,6 +15,8 @@ function draw(){
     var noOfDots = 20;
     var size = width/noOfDots;
 
+    translate(0,size);
+
     for (var x = 0; x < noOfDots; x++){
       for (var y = 0; y < noOfDots; y++){
 
@@ -27,44 +29,71 @@ function draw(){
         var c = color(colorRandom,0,colorRandom)
 
         // phase angle
-        phaseRatio = mouseX / width * 4;
-        phaseAngle = ( frameCount + locX + locY ) * phaseRatio
+        var phaseAngle = calculatePhaseAngle(locX,locY)
 
         // connecting dots
-        drawConnectingDots(x,y,noOfDots,size/2,phaseAngle)
+        drawConnectingDots(x,y,noOfDots,size/2)
 
         push()
         // animation effect
         translate(locX,locY)
         rotate(phaseAngle)
         // Create a wave
-        wave(size/2,size/2,size/2,c);
+        wave(size/2,0,size/2,c);
         pop()
       }
     }
-    
 }
 
-function drawConnectingDots(x,y,noOfDots,size,phaseAngle){
+function calculatePhaseAngle(x,y){
+  
+  if(x == null || y == null) return 0
 
-  var x1 = x * width / noOfDots;
+  var phaseRatio = mouseX / width * 4
+
+  return ( frameCount + x + y ) * phaseRatio
+}
+
+function drawConnectingDots(x,y,noOfDots,size){
+
+  var x1 = x * width / noOfDots ;
   var y1 = y * height / noOfDots;
   var x2;
   var y2;
-  push()
-  translate(size,size)
+
+  var phaseAngle1 = calculatePhaseAngle(x1,y1)
+  var phaseAngle2;
 
   // vertical connecting lines
-  x2 = x * width / noOfDots;
-  y2 = y+1 * height / noOfDots;
-  line(x1,y1,x2,y2)
+  if(y+1<noOfDots)
+  {
+    x2 = x * width / noOfDots;
+    y2 = (y+1) * height / noOfDots;
 
-  // horizontal connecting lines
-  x2 = x+1 * width / noOfDots;
-  y2 = y * height / noOfDots;
-  line(x1,y1,x2,y2)
-  pop()
+    phaseAngle2 = calculatePhaseAngle(x2,y2)
 
+    line(
+      x1 + cos(phaseAngle1)*size,
+      y1 + sin(phaseAngle1)*size,
+      x2 + cos(phaseAngle2)*size,
+      y2 + sin(phaseAngle2)*size)
+
+  }
+ 
+  //horizontal connecting lines
+  if(x+1<noOfDots)
+  {
+    x2 = (x+1) * width / noOfDots;
+    y2 = y * height / noOfDots
+
+    phaseAngle2 = calculatePhaseAngle(x2,y2)
+
+    line(
+      x1 + cos(phaseAngle1)*size,
+      y1 + sin(phaseAngle1)*size,
+      x2 + cos(phaseAngle2)*size,
+      y2 + sin(phaseAngle2)*size)
+  }
 }
 
 
