1ce7e413f49f7f0eff2cfcb4fcedb493dbcdbcef
egmp7 <deweyerick@gmail.com>
2023-06-13 12:36:10 -0500

    wavedots done

--- JS/p5/wavingDots/sketch.js
+++ JS/p5/wavingDots/sketch.js
@@ -1,22 +1,20 @@
-
+var time = 100;
 
 function setup(){
     createCanvas(500, 500);
-    background(255);
     randomSeed(0)
     angleMode(DEGREES)
-    
 }
 
 function draw(){
 
-    background(255);
+    background(0);
+    translate(0,size);
     
     var noOfDots = 20;
     var size = width/noOfDots;
 
-    translate(0,size);
-
+    
     for (var x = 0; x < noOfDots; x++){
       for (var y = 0; y < noOfDots; y++){
 
@@ -25,8 +23,8 @@ function draw(){
         var locY = y * height / noOfDots;
 
         // Generate a Color
-        colorRandom = random(0,255)
-        var c = color(colorRandom,0,colorRandom)
+        colorRandom = random(100,255)
+        var c = noiseColorGenerator()
 
         // phase angle
         var phaseAngle = calculatePhaseAngle(locX,locY)
@@ -45,6 +43,12 @@ function draw(){
     }
 }
 
+/** Calculates the phase angle depending 
+ * on the position of the Mouse, frameCount, 
+ * x & y position
+ * @param x int coordenate
+ * @param y int coordenate
+ * @returns float */
 function calculatePhaseAngle(x,y){
   
   if(x == null || y == null) return 0
@@ -54,8 +58,20 @@ function calculatePhaseAngle(x,y){
   return ( frameCount + x + y ) * phaseRatio
 }
 
-function drawConnectingDots(x,y,noOfDots,size){
+/** 
+ * Algorithm to connect each of the ellipses of the wave 
+ * with a line function
+ * 
+ * @param x coordenate x from each ellipse of the wave,
+ * @param y coordenate y from each ellipse of the wave,
+ * @param noOfDots total number of ellipse in one row,
+ * @param amplitude defined variable size from the draw function
+ */
+function drawConnectingDots(x,y,noOfDots,amplitude){
 
+  stroke(noiseColorGenerator())
+
+  // get first point
   var x1 = x * width / noOfDots ;
   var y1 = y * height / noOfDots;
   var x2;
@@ -67,40 +83,51 @@ function drawConnectingDots(x,y,noOfDots,size){
   // vertical connecting lines
   if(y+1<noOfDots)
   {
-    x2 = x * width / noOfDots;
+    // get second point
+    x2 = x1;
     y2 = (y+1) * height / noOfDots;
 
     phaseAngle2 = calculatePhaseAngle(x2,y2)
 
     line(
-      x1 + cos(phaseAngle1)*size,
-      y1 + sin(phaseAngle1)*size,
-      x2 + cos(phaseAngle2)*size,
-      y2 + sin(phaseAngle2)*size)
-
+      x1 + cos(phaseAngle1)*amplitude,
+      y1 + sin(phaseAngle1)*amplitude,
+      x2 + cos(phaseAngle2)*amplitude,
+      y2 + sin(phaseAngle2)*amplitude)
   }
  
   //horizontal connecting lines
   if(x+1<noOfDots)
   {
+    // get second point
     x2 = (x+1) * width / noOfDots;
-    y2 = y * height / noOfDots
+    y2 = y1;
 
     phaseAngle2 = calculatePhaseAngle(x2,y2)
 
     line(
-      x1 + cos(phaseAngle1)*size,
-      y1 + sin(phaseAngle1)*size,
-      x2 + cos(phaseAngle2)*size,
-      y2 + sin(phaseAngle2)*size)
+      x1 + cos(phaseAngle1)*amplitude,
+      y1 + sin(phaseAngle1)*amplitude,
+      x2 + cos(phaseAngle2)*amplitude,
+      y2 + sin(phaseAngle2)*amplitude)
   }
 }
 
-
+/** Draws an ellipse */
 function wave(x,y,size,color) {
  
   noStroke()
   fill(color)
   ellipse(x,y,size)
   
+}
+
+/** Generates shades of purple through noise
+ * @returns color
+ */
+function noiseColorGenerator(){
+  var nX = noise(time)
+  nX = map(nX,0,1,100,250)
+  time +=10
+  return color(nX,0,nX)
 }
