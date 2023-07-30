0ea021c553ba4d9302f4cabbbb9ce23fc8238fd8
egmp7 <deweyerick@gmail.com>
2023-07-29 20:51:37 -0500

    checkboxes

--- JS/p5/instagramFilter/sketch.js
+++ JS/p5/instagramFilter/sketch.js
@@ -11,6 +11,14 @@ var matrix = [
     [1/64, 1/64, 1/64, 1/64, 1/64, 1/64, 1/64, 1/64],
     [1/64, 1/64, 1/64, 1/64, 1/64, 1/64, 1/64, 1/64]
 ];
+/**************************
+ * My code starts here
+ **************************/
+var checkboxes={}
+/**************************
+ * My code ends here
+ **************************/
+
 /////////////////////////////////////////////////////////////////
 function preload() {
     imgIn = loadImage("assets/husky.jpg");
@@ -18,6 +26,20 @@ function preload() {
 /////////////////////////////////////////////////////////////////
 function setup() {
     createCanvas((imgIn.width * 2), imgIn.height);
+    /**************************
+     * My code starts here
+     **************************/
+    checkboxes.sepiaFilter = createCheckbox('Sepia Filter', false)
+	checkboxes.sepiaFilter.changed( sepiaFilterEvent )
+	checkboxes.darkCorners = createCheckbox('Dark Corners', false)
+	checkboxes.darkCorners.changed( darkCornersEvent )
+	checkboxes.radialBlurFilter = createCheckbox('Radial Blur Filter', false)
+	checkboxes.radialBlurFilter.changed ( radialBlurFilterEvent )
+	checkboxes.borderFilter = createCheckbox('Border Filter', false)
+	checkboxes.borderFilter.changed( borderFilterEvent )
+    /**************************
+     * My code ends here
+     **************************/
 }
 /////////////////////////////////////////////////////////////////
 function draw() {
@@ -26,19 +48,46 @@ function draw() {
     image(earlyBirdFilter(imgIn), imgIn.width, 0);
     noLoop();
 }
-/////////////////////////////////////////////////////////////////
-function mousePressed(){
-  loop();
-}
+
 /////////////////////////////////////////////////////////////////
 function earlyBirdFilter(img){
-  var resultImg = createImage(imgIn.width, imgIn.height);
-  resultImg = sepiaFilter(imgIn);
-  resultImg = darkCorners(resultImg);
-  resultImg = radialBlurFilter(resultImg);
-  resultImg = borderFilter(resultImg)
-  return resultImg;
+
+	var resultImg = createImage(imgIn.width, imgIn.height);
+	resultImg = img;
+
+	// filters
+	if( checkboxes.sepiaFilter.checked() ) resultImg = sepiaFilter(imgIn);
+	if( checkboxes.darkCorners.checked() ) resultImg = darkCorners(resultImg);
+	if( checkboxes.radialBlurFilter.checked() ) resultImg = radialBlurFilter(resultImg);
+	if( checkboxes.borderFilter.checked() )resultImg = borderFilter(resultImg)
+
+	return resultImg;
 }
+
+function convolution(x,y,matrix,matrixSize,img){
+  var totalRed = 0.0;
+  var totalGreen = 0.0;
+  var totalBlue = 0.0;
+  var offset = floor(matrixSize / 2);
+  
+  for(var i = 0 ; i < matrixSize; i++){
+    for(var j = 0 ; j < matrixSize; j++){
+        var xloc = x + i - offset;
+        var yloc = y + j - offset;
+
+        var index = (xloc + img.width * yloc) * 4;
+        index = constrain(index, 0, img.pixels.length - 1);
+
+        totalRed += img.pixels[index + 0] * matrix[i][j];
+        totalGreen += img.pixels[index + 1] * matrix[i][j];
+        totalBlue += img.pixels[index + 2] * matrix[i][j];
+    }
+	}
+
+	return [totalRed,totalGreen,totalBlue]
+}
+
+/////////////////////////////////////////////////////////////////
 /**************************
  * My code starts here
  **************************/
@@ -85,50 +134,50 @@ function sepiaFilter(img){
  * @returns {p5.Image} Dark Corners image
  */
 function darkCorners(img){
-  var imgOut = createImage(img.width, img.height);
-  const maxDistanceFromCenter = dist(0,0,img.width/2,img.height/2)
-  img.loadPixels();
-  imgOut.loadPixels();
+	var imgOut = createImage(img.width, img.height);
+	const maxDistanceFromCenter = dist(0,0,img.width/2,img.height/2)
+	img.loadPixels();
+	imgOut.loadPixels();
 
-  // read every pixel
-  for (var x = 0; x < img.width; x++) {
-    for (var y = 0; y < img.height; y++) {
+	// read every pixel
+	for (var x = 0; x < img.width; x++) {
+		for (var y = 0; y < img.height; y++) {
 
-      var distanceFromCenter = dist(x,y,img.width/2,img.height/2)
-      var index = (x + y * img.width) * 4;
+		var distanceFromCenter = dist(x,y,img.width/2,img.height/2)
+		var index = (x + y * img.width) * 4;
 
-      // get RGB colors
-      var r = img.pixels[index + 0];
-      var g = img.pixels[index + 1];
-      var b = img.pixels[index + 2];
+		// get RGB colors
+		var r = img.pixels[index + 0];
+		var g = img.pixels[index + 1];
+		var b = img.pixels[index + 2];
 
-      var dynLum;
+		var dynLum;
 
-      // dark at 300 points from center
-      if (distanceFromCenter >= 300){
-        dynLum = map(distanceFromCenter,300,450,1,0.4);
-        r = r * dynLum;
-        g = g * dynLum;
-        b = b * dynLum;
-      }
+		// dark at 300 points from center
+		if (distanceFromCenter >= 300){
+			dynLum = map(distanceFromCenter,300,450,1,0.4);
+			r = r * dynLum;
+			g = g * dynLum;
+			b = b * dynLum;
+		}
 
-      //dark at 450 points from center
-      else if (distanceFromCenter >= 450){
-        dynLum = map(distanceFromCenter,450,maxDistanceFromCenter,0.4,0);
-        r = r * dynLum;
-        g = g * dynLum;
-        b = b * dynLum;
-      }
-  
-      // set RGB colors
-      imgOut.pixels[index + 0] = r;
-      imgOut.pixels[index + 1] = g;
-      imgOut.pixels[index + 2] = b;
-      imgOut.pixels[index + 3] = 255;
-    }
-  }
-  imgOut.updatePixels();
-  return imgOut;
+		//dark at 450 points from center
+		else if (distanceFromCenter >= 450){
+			dynLum = map(distanceFromCenter,450,maxDistanceFromCenter,0.4,0);
+			r = r * dynLum;
+			g = g * dynLum;
+			b = b * dynLum;
+		}
+	
+		// set RGB colors
+		imgOut.pixels[index + 0] = r;
+		imgOut.pixels[index + 1] = g;
+		imgOut.pixels[index + 2] = b;
+		imgOut.pixels[index + 3] = 255;
+		}
+	}
+	imgOut.updatePixels();
+	return imgOut;
 }
 
 /**
@@ -138,46 +187,46 @@ function darkCorners(img){
  * @returns {p5.Image} Blured Image
  */
 function radialBlurFilter(img){
-  var imgOut = createImage(img.width, img.height);
-  var matrixSize = matrix.length;
-  img.loadPixels();
-  imgOut.loadPixels();
+	var imgOut = createImage(img.width, img.height);
+	var matrixSize = matrix.length;
+	img.loadPixels();
+	imgOut.loadPixels();
 
-  // get mousePosition from color image
-  const mousePosition = createVector(
-    constrain(mouseX,0,imgIn.width),
-    constrain(mouseY,0,imgIn.height))
+	// get mousePosition from color image
+	const mousePosition = createVector(
+		constrain(mouseX,0,imgIn.width),
+		constrain(mouseY,0,imgIn.height))
 
-  // read every pixel
-  for (var x = 0; x < img.width; x++) {
-    for (var y = 0; y < img.height; y++) {
+	// read every pixel
+	for (var x = 0; x < img.width; x++) {
+		for (var y = 0; y < img.height; y++) {
 
-      var index = (x + y * img.width) * 4;
+		var index = (x + y * img.width) * 4;
 
-      // get RGB colors from param img
-      var r = img.pixels[index + 0];
-      var g = img.pixels[index + 1];
-      var b = img.pixels[index + 2];
+		// get RGB colors from param img
+		var r = img.pixels[index + 0];
+		var g = img.pixels[index + 1];
+		var b = img.pixels[index + 2];
 
-      // calculate dynBlur
-      var dynBlur;
-      var distanceMouseToPixel = dist(mousePosition.x,mousePosition.y,x,y)
-      if (distanceMouseToPixel < 100) dynBlur = 0;
-      else dynBlur = map(constrain(distanceMouseToPixel,100,300),100,300,0,1);
-      
-      // convolution
-      var c = convolution(x,y,matrix,matrixSize,img)
+		// calculate dynBlur
+		var dynBlur;
+		var distanceMouseToPixel = dist(mousePosition.x,mousePosition.y,x,y)
+		if (distanceMouseToPixel < 100) dynBlur = 0;
+		else dynBlur = map(constrain(distanceMouseToPixel,100,300),100,300,0,1);
+		
+		// convolution
+		var c = convolution(x,y,matrix,matrixSize,img)
 
-      // set RGB colors
-      imgOut.pixels[index + 0] = c[0]*dynBlur + r*(1-dynBlur);
-      imgOut.pixels[index + 1] = c[1]*dynBlur + g*(1-dynBlur);
-      imgOut.pixels[index + 2] = c[2]*dynBlur + b*(1-dynBlur);
-      imgOut.pixels[index + 3] = 255;
-    }
-  }
+		// set RGB colors
+		imgOut.pixels[index + 0] = c[0]*dynBlur + r*(1-dynBlur);
+		imgOut.pixels[index + 1] = c[1]*dynBlur + g*(1-dynBlur);
+		imgOut.pixels[index + 2] = c[2]*dynBlur + b*(1-dynBlur);
+		imgOut.pixels[index + 3] = 255;
+		}
+	}
 
-  imgOut.updatePixels();
-  return imgOut;
+	imgOut.updatePixels();
+	return imgOut;
 }
 
 /**
@@ -187,53 +236,34 @@ function radialBlurFilter(img){
  */
 function borderFilter(img){
 
-  var buffer = createGraphics(img.width,img.height)
-  buffer.image(img,0,0);
-  
-  // rounded border
-  var weight = 10;
-  buffer.strokeWeight(weight)
-  buffer.stroke(255);
-  buffer.fill(0,0,0,0);
-  buffer.rect(weight/2,weight/2,img.width -weight/2,img.height-weight/2,50);
-  buffer.rect(weight/2,weight/2,img.width -weight/2,img.height-weight/2);
+	var buffer = createGraphics(img.width,img.height)
+	buffer.image(img,0,0);
+	
+	// rounded border
+	var weight = 20;
+	buffer.strokeWeight(weight)
+	buffer.stroke(255);
+	buffer.fill(0,0,0,0);
+	buffer.rect(weight/2,weight/2,img.width -weight/2,img.height-weight/2,50);
+	buffer.rect(weight/2,weight/2,img.width -weight/2,img.height-weight/2);
 
-  return buffer;
+	return buffer;
 }
 
-function mousePressed(){
-  loop()
-}
-function mouseReleased(){
-  noLoop()
-}
+/////////////////////////////////////////////////////////////////
+// EVENTS
+
+function sepiaFilterEvent(){ loop(); }
+function darkCornersEvent(){ loop(); }
+function radialBlurFilterEvent(){ loop(); }
+function borderFilterEvent(){ loop(); }
+function mousePressed(){ loop(); }
 
 /**************************
  * My code ends here
  **************************/
 
-function convolution(x,y,matrix,matrixSize,img){
-  var totalRed = 0.0;
-  var totalGreen = 0.0;
-  var totalBlue = 0.0;
-  var offset = floor(matrixSize / 2);
-  
-  for(var i = 0 ; i < matrixSize; i++){
-    for(var j = 0 ; j < matrixSize; j++){
-        var xloc = x + i - offset;
-        var yloc = y + j - offset;
 
-        var index = (xloc + img.width * yloc) * 4;
-        index = constrain(index, 0, img.pixels.length - 1);
-
-        totalRed += img.pixels[index + 0] * matrix[i][j];
-        totalGreen += img.pixels[index + 1] * matrix[i][j];
-        totalBlue += img.pixels[index + 2] * matrix[i][j];
-    }
-}
-
-return [totalRed,totalGreen,totalBlue]
-}
 
 
 
