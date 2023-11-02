accb83f03be40d3d9a6b698eb9b1289f16b8c9d7
Erick Gonzalez <deweyerick@gmail.com>
2023-11-02 14:38:33 -0500

    Update and rename CreateBullet.cs to InstantiateGameObject.cs

--- Unity/Topic-3-Physics/InstantiateGameObject.cs
+++ Unity/Topic-3-Physics/InstantiateGameObject.cs
@@ -3,23 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Mounted in SpawnPosition GameObject
-public class CreateBullet : MonoBehaviour
+public class InstantiateGameObject : MonoBehaviour
 {
     public GameObject bullet;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
             Instantiate(bullet, transform.position, transform.rotation);
-        }
-        
+        } 
     }
 }
