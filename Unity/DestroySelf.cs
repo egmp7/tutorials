27c9a23988a37952024a2593da12c5625c0f1bdb
egmp7 <deweyerick@gmail.com>
2023-12-26 21:33:08 -0600

    testing

--- Unity/DestroySelf.cs
+++ Unity/DestroySelf.cs
@@ -7,13 +7,11 @@ public class DestroySelf : MonoBehaviour
 {
     [SerializeField]
     float seconds;
-    // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, seconds);
     }
 
-    // Update is called once per frame
     void Update()
     {
 
